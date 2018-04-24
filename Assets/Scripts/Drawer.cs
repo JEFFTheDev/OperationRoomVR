2dd1a3a1931d47befe00a17cb23bf17d0b7946d2
Jeffrey <osta0004@hz.nl>
2018-04-24 14:39:08 +0200

    Added cart prototype

---
+++ Assets/Scripts/Drawer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour {
+
+    public Vector3 openPos;
+    public float animSpeed;
+    private Vector3 closePos;
+    private bool isClosed;
+
+	// Use this for initialization
+	void Start () {
+        isClosed = true;
+        closePos = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, isClosed ? closePos : openPos, animSpeed * Time.deltaTime);
+	}
+
+    public void OpenOrClose()
+    {
+        isClosed = !isClosed;
+    }
+}
---
+++ Assets/Scripts/Interactible.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactible : MonoBehaviour {
+
+    public UnityEvent onGrab;
+    public UnityEvent onRelease;
+    public string[] tags;
+    private GameObject collidedWith;
+    private bool isGrabbed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (HasTag(col.gameObject) && !isGrabbed)
+        {
+            collidedWith = col.gameObject;
+            isGrabbed = true;
+            onGrab.Invoke();
+            Debug.Log("Colliding with: " + col.gameObject.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject == collidedWith)
+        {
+            collidedWith = null;
+            isGrabbed = false;
+            onRelease.Invoke();
+            Debug.Log("Stopped colliding with: " + col.gameObject.name);
+        }
+    }
+
+    private bool HasTag(GameObject hasTag)
+    {
+        foreach(string t in tags)
+        {
+            if(hasTag.tag == t)
+                return true;
+        }
+
+        return false;
+    }
+}
