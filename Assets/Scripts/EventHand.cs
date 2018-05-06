9366b0d3443b4b290a4ade30c63bdbc913224ed4
osta0004 <osta0004@hz.nl>
2018-05-06 17:46:48 +0200

    Changed drawers, started work on new eventsystem

---
+++ Assets/Scripts/EventHand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHand : MonoBehaviour {
+
+    //Event messages
+    private const string grabMessage = "OnGrab";
+    private const string releaseMessage = "OnRelease";
+    private const string holdMessage = "OnHold";
+    private const string touchMessage = "OnTouch";
+    private const string gazeMessage = "OnGaze";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        SendEventMessage(col.gameObject, touchMessage);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        
+    }
+
+    public void SendEventMessage(GameObject sendTo, string message)
+    {
+        //Send event message to object and this hand transform with it
+        sendTo.SendMessage(message, transform, SendMessageOptions.DontRequireReceiver);
+    }
+}
