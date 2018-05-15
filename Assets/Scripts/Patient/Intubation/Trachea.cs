cc6ea73201ada652eeec06be9508832c578159bb
Jeffrey <osta0004@hz.nl>
2018-05-15 12:32:00 +0200

    Added damage effect, trachea tube interaction

---
+++ Assets/Scripts/Patient/Intubation/Trachea.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trachea : MonoBehaviour {
+
+    public GameObject dropPrefab;
+    public float dropYPos;
+    public GameObject marker;
+    public GameObject vocalCords;
+    public float allowedDistance;
+    public TextMesh finishText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsObjectBetweenMarkers(marker) && !finishText.gameObject.activeSelf)
+        {
+            finishText.gameObject.SetActive(true);
+            Debug.Log("Tube in position!");
+        }
+        else if (finishText.gameObject.activeSelf)
+        {
+            finishText.gameObject.SetActive(false);
+        }
+	}
+
+    private bool IsObjectBetweenMarkers(GameObject g)
+    {
+        return Vector3.Distance(g.transform.position, vocalCords.transform.position) < allowedDistance;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Touched trachea!");
+        Vector3 dropPos = col.contacts[0].point;
+        dropPos.y += dropYPos;
+        SpawnDrop(dropPos);
+    }
+
+    private void SpawnDrop(Vector3 pos)
+    {
+        GameObject drop = GameObject.Instantiate(dropPrefab);
+        drop.transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Patient/Intubation/WaterDrop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDrop : MonoBehaviour {
+
+    public float speed;
+    public Projector projector;
+    public float stopAt;
+
+	// Update is called once per frame
+	void Update () {
+
+        //Lerp fov value closer to 0
+        projector.fieldOfView = Mathf.Lerp(projector.fieldOfView, 0, speed * Time.deltaTime);
+        
+        if (IsWithinRange())
+            Destroy(this.gameObject);
+	}
+
+
+    //Return if projector fov is smaller than or equal to value where animation needs to stop, return true
+    private bool IsWithinRange()
+    {
+        return projector.fieldOfView <= stopAt;
+    }
+}
