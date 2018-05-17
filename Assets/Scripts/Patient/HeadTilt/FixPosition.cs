9aebbaf255a1ec5958f1a9cd0cb7d9c21273476d
guillegard <guillegard95@gmail.com>
2018-05-17 05:34:58 -0500

    HeadTilt

---
+++ Assets/Scripts/Patient/HeadTilt/FixPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPosition : MonoBehaviour {
+
+    private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = pos;
+	}
+}
