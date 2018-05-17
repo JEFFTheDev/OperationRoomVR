3c1621baec28c854cb4dcbad7e33cdaa52bf9d44
Veso <vess.chumpalov@gmail.com>
2018-05-17 11:57:29 +0200

    laryngoscope folding, trachea and vocal cords test

---
+++ Assets/Scripts/EventSystem/LaryngoscopeFolding.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaryngoscopeFolding : MonoBehaviour {
+
+    private bool unfold=false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && unfold == false)
+        {
+            this.transform.Rotate(transform.up, 68.873f);
+        }
+            
+    }
+}
