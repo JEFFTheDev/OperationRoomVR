d105e4a66c069588ac032e751f833cc97ca18c65
osta0004 <osta0004@hz.nl>
2018-05-20 15:15:34 +0200

    Working xray system

--- Assets/Scripts/XRaySystem/XRayObject.cs
+++ Assets/Scripts/XRaySystem/XRayObject.cs
@@ -1,14 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class XRayObject : MonoBehaviour {
-    
+
+    private enum WorkflowMode
+    {
+        Specular,
+        Metallic,
+        Dielectric
+    }
+
+    public enum BlendMode
+    {
+        Opaque,
+        Cutout,
+        Fade,   // Old school alpha-blending mode, fresnel does not affect amount of transparency
+        Transparent // Physically plausible transparency mode, implemented as alpha pre-multiply
+    }
+
+    public enum SmoothnessMapChannel
+    {
+        SpecularMetallicAlpha,
+        AlbedoAlpha,
+    }
+
     //Shader names
     private const string xRay = "Custom/Stencil/XRay";
     private const string xRayIgnore = "Custom/Stencil/XRayIgnore";
     private const string xRayReveal = "Custom/Stencil/XRayContents";
     private const string xRayIgnoreRenderer = "Custom/Stencil/XRayIgnoreRenderer";
+    
 
     //Shaders
     private Shader xRayShader;
@@ -16,7 +39,7 @@ public class XRayObject : MonoBehaviour {
     private Shader xRayRevealShader;
     private Shader xRayIgnoreRendShader;
     
-    private const float alpha = 75;
+    private const float alpha = .4f;
 
     public bool isTransparent;
     
@@ -44,19 +67,30 @@ public class XRayObject : MonoBehaviour {
             renderLayer.transform.position = pos;
 
             renderLayer.transform.rotation = transform.rotation;
-
-            //Set renderlayers renderqueue to 2000 so that xray will have priority over it
-            SetRenderQueue(renderLayer, 2000);
             
             //Make this layer ignored by xray
             ApplyShader(this.gameObject, xRayIgnoreShader);
 
+            //Set render queue of this object above renderlayer or it won't render
+            //SetRenderQueue(this.gameObject, 2001);
+
             transparentLayer.transform.SetParent(this.transform);
             renderLayer.transform.SetParent(this.transform);
+
+            //Set renderlayers renderqueue to 2001 so that xray will have priority over it
+            SetRenderQueue(renderLayer, 1999);
+            //SetRenderQueue(transparentLayer, 2000);
+            //SetRenderQueue(this.gameObject, 1999);
+
+            //UpdateMaterials(renderLayer);
+            UpdateMaterials(transparentLayer);
+            UpdateMaterials(this.gameObject);
         }
         else
         {
             ApplyShader(this.gameObject, xRayRevealShader);
+            //SetRenderQueue(this.gameObject, 2000);
+            UpdateMaterials(this.gameObject);
         }
     }
 	
@@ -85,9 +119,10 @@ public class XRayObject : MonoBehaviour {
     {
         foreach(MeshRenderer m in g.GetComponentsInChildren<MeshRenderer>())
         {
+            m.material.SetFloat("_Mode", 3);
             Color c = m.material.color;
             c.a = alpha;
-            m.material.SetColor("_MainColor", c);
+            m.material.color = c;
         }
     }
 
@@ -95,7 +130,10 @@ public class XRayObject : MonoBehaviour {
     {
         foreach (MeshRenderer m in g.GetComponentsInChildren<MeshRenderer>())
         {
-            m.material.shader = shader;
+            Material copy = new Material(shader);
+            copy.CopyPropertiesFromMaterial(m.material);
+            //copy.shader = shader;
+            m.material = copy;
         }
     }
     
@@ -113,5 +151,109 @@ public class XRayObject : MonoBehaviour {
         xRayIgnoreShader = Shader.Find(xRayIgnore);
         xRayRevealShader = Shader.Find(xRayReveal);
         xRayIgnoreRendShader = Shader.Find(xRayIgnoreRenderer);
+    }
+
+    private void UpdateMaterials(GameObject g)
+    {
+        foreach (MeshRenderer m in g.GetComponentsInChildren<MeshRenderer>())
+        {
+            MaterialChanged(m.material, WorkflowMode.Metallic);
+        }
+    }
+
+    public static void SetupMaterialWithBlendMode(Material material, BlendMode blendMode)
+    {
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                material.SetOverrideTag("RenderType", "");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                //material.renderQueue = -1;
+                break;
+            case BlendMode.Cutout:
+                material.SetOverrideTag("RenderType", "TransparentCutout");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                material.SetInt("_ZWrite", 1);
+                material.EnableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                //material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.AlphaTest;
+                break;
+            case BlendMode.Fade:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.EnableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                //material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+            case BlendMode.Transparent:
+                material.SetOverrideTag("RenderType", "Transparent");
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.DisableKeyword("_ALPHABLEND_ON");
+                material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                //material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+                break;
+        }
+    }
+
+    static SmoothnessMapChannel GetSmoothnessMapChannel(Material material)
+    {
+        int ch = (int)material.GetFloat("_SmoothnessTextureChannel");
+        if (ch == (int)SmoothnessMapChannel.AlbedoAlpha)
+            return SmoothnessMapChannel.AlbedoAlpha;
+        else
+            return SmoothnessMapChannel.SpecularMetallicAlpha;
+    }
+
+    static void SetMaterialKeywords(Material material, WorkflowMode workflowMode)
+    {
+        // Note: keywords must be based on Material value not on MaterialProperty due to multi-edit & material animation
+        // (MaterialProperty value might come from renderer material property block)
+        SetKeyword(material, "_NORMALMAP", material.GetTexture("_BumpMap") || material.GetTexture("_DetailNormalMap"));
+        if (workflowMode == WorkflowMode.Specular)
+            Debug.Log("Continue");//SetKeyword(material, "_SPECGLOSSMAP", material.GetTexture("_SpecGlossMap"));
+        else if (workflowMode == WorkflowMode.Metallic)
+            SetKeyword(material, "_METALLICGLOSSMAP", material.GetTexture("_MetallicGlossMap"));
+        SetKeyword(material, "_PARALLAXMAP", material.GetTexture("_ParallaxMap"));
+        SetKeyword(material, "_DETAIL_MULX2", material.GetTexture("_DetailAlbedoMap") || material.GetTexture("_DetailNormalMap"));
+
+        // A material's GI flag internally keeps track of whether emission is enabled at all, it's enabled but has no effect
+        // or is enabled and may be modified at runtime. This state depends on the values of the current flag and emissive color.
+        // The fixup routine makes sure that the material is in the correct state if/when changes are made to the mode or color.
+        MaterialEditor.FixupEmissiveFlag(material);
+        bool shouldEmissionBeEnabled = (material.globalIlluminationFlags & MaterialGlobalIlluminationFlags.EmissiveIsBlack) == 0;
+        SetKeyword(material, "_EMISSION", shouldEmissionBeEnabled);
+
+        if (material.HasProperty("_SmoothnessTextureChannel"))
+        {
+            SetKeyword(material, "_SMOOTHNESS_TEXTURE_ALBEDO_CHANNEL_A", GetSmoothnessMapChannel(material) == SmoothnessMapChannel.AlbedoAlpha);
+        }
+    }
+
+    static void MaterialChanged(Material material, WorkflowMode workflowMode)
+    {
+        SetupMaterialWithBlendMode(material, (BlendMode)material.GetFloat("_Mode"));
+
+        SetMaterialKeywords(material, workflowMode);
+    }
+
+    static void SetKeyword(Material m, string keyword, bool state)
+    {
+        if (state)
+            m.EnableKeyword(keyword);
+        else
+            m.DisableKeyword(keyword);
     }
 }
