1a3c0056782eeebcc3a4516405ff9386782bbb0b
guillegard <guillegard95@gmail.com>
2018-05-23 04:00:27 -0500

    Fold and unfold laryngoscope/ first tilthead fix

--- Assets/Scripts/Patient/HeadTilt/HeadTilt.cs
+++ Assets/Scripts/Patient/HeadTilt/HeadTilt.cs
@@ -9,18 +9,34 @@ using System.Collections;
 public class HeadTilt : MonoBehaviour
 {
 
-    protected Animator animator;
+    Animator animator;
     public Transform lookObj = null;
+    private List<Animator> layers;
+    Animator trans;
+    Animator rend;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
-    }
+        animator = this.GetComponent<Animator>();
+        //layers = GetComponentsInChildren<Animator>();
 
+    }
+    private void Update()
+    {
+        trans = GameObject.Find("TransparentLayer").GetComponent<Animator>();
+        rend = GameObject.Find("RenderLayer").GetComponent<Animator>();
+    }
     //a callback for calculating IK
     void OnAnimatorIK()
     {
+
         animator.SetLookAtWeight(1);
         animator.SetLookAtPosition(lookObj.position);
+
+        trans.SetLookAtWeight(1);
+        trans.SetLookAtPosition(lookObj.position);
+
+        rend.SetLookAtWeight(1);
+        rend.SetLookAtPosition(lookObj.position);
     }
 }
