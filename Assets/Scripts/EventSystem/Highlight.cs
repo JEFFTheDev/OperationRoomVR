ebf9f599fb9d9152f2567ba966fff1fe1e8536c3
Jeffrey <osta0004@hz.nl>
2018-05-23 11:25:27 +0200

    Added highlight

---
+++ Assets/Scripts/EventSystem/Highlight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour, IInteractable {
+
+    private bool isHighlighted;
+    private Shader startShader;
+    
+    public void OnTouch(Transform hand)
+    {
+        HighlightObject(true);
+    }
+
+    public void OnTouchStop(Transform hand)
+    {
+        HighlightObject(false);
+    }
+
+    private void HighlightObject(bool hightlight)
+    {
+        Material m = GetComponent<MeshRenderer>().material;
+        m.shader = hightlight ? Shader.Find("Outlined/Silhouetted Diffuse") : startShader;
+        isHighlighted = hightlight;
+    }
+
+    public void OnGrab(Transform hand)
+    {
+        //No implementation needed
+    }
+
+    public void OnRelease(Transform hand)
+    {
+        //No implementation needed
+    }
+
+}
