61eeb5b96223bbd901774e64082e8a42c6c574bd
Jeffrey <osta0004@hz.nl>
2018-06-12 12:03:43 +0200

    Added moveable cart script, updated lock script

--- Assets/Scripts/Environment/Lock.cs
+++ Assets/Scripts/Environment/Lock.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class Lock : MonoBehaviour {
 
-    public bool xPos, yPos, zPos;
-    public Vector3 maxLocPos;
-    private Vector3 startWorldPos;
-    private Transform startParent;
+    public bool lockLocX, lockLocY, lockLocZ;
+    public Vector3 maxPos;
+    public Vector3 minPos;
+    private Vector3 localStartPos;
 
 	// Use this for initialization
 	void Start () {
-        startWorldPos = transform.position;
-        startParent = transform.parent;
+        localStartPos = transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
+        //Lock position to set variables
+        Vector3 pos = transform.localPosition;
+        pos.x = lockLocX ? localStartPos.x : Clamp(pos.x, minPos.x, maxPos.x);
+        pos.y = lockLocY ? localStartPos.y : Clamp(pos.y, minPos.y, maxPos.y);
+        pos.z = lockLocZ ? localStartPos.z : Clamp(pos.z, minPos.z, maxPos.z);
+        transform.localPosition = pos;
 
-        Vector3 pos = transform.position;
-        pos.x = xPos ? startWorldPos.x : pos.x;
-        pos.y = yPos ? startWorldPos.y : pos.y;
-        pos.z = zPos ? startWorldPos.z : pos.z;
-        transform.position = pos;
+    }
 
+    private float Clamp(float value, float min, float max)
+    {
+        float actualMin = min < max ? min : max;
+        float actualMax = min > max ? min : max;
+        return Mathf.Clamp(value, actualMin, actualMax);
     }
 }
---
+++ Assets/Scripts/Equipment/AnesthesiaCart/MovableCart.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableCart : MonoBehaviour {
+
+    private bool isMoving;
+    private Transform hand;
+    public float speed;
+    public float wheelsRotateSpeed;
+    public GameObject[] wheels;
+    private Vector3 direction;
+    private Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hand)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, hand.position, speed * Time.deltaTime);
+        }
+
+        direction = transform.position - lastPosition;
+
+        lastPosition = transform.position;
+
+
+        //Rotate wheels
+        foreach (GameObject wheel in wheels)
+        {
+            Vector3 rot = Vector3.RotateTowards(transform.forward, direction, wheelsRotateSpeed * Time.deltaTime, 0f);
+            wheel.transform.rotation = Quaternion.LookRotation(rot);
+        }
+    }
+
+    void OnGrabEvent(Transform hand)
+    {
+        this.hand = hand;
+    }
+
+    void OnReleaseEvent()
+    {
+        hand = null;
+    }
+}
