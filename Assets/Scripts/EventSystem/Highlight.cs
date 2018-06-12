57396eaf1acfa650e7ef55bf56cff619951af587
Jeffrey <osta0004@hz.nl>
2018-06-12 10:17:19 +0200

    Worked on highlight

--- Assets/Scripts/EventSystem/Highlight.cs
+++ Assets/Scripts/EventSystem/Highlight.cs
@@ -6,31 +6,77 @@ using UnityEngine;
 public class Highlight : MonoBehaviour, IInteractable {
 
     private bool isHighlighted;
-    private Shader startShader;
+    private GameObject combinedObject;
+    private Dictionary<Material, Shader> materialLibrary;
 
     private void Start()
     {
-        startShader = GetComponent<MeshRenderer>().material.shader;
+        materialLibrary = new Dictionary<Material, Shader>();
     }
     
     public void OnTouch(Transform hand)
     {
-        HighlightObject(true);
+        Debug.Log("test");
+
+        if (HasMoreMeshes() && combinedObject == null)
+            combinedObject = CreateCombinedObject();
+
+        HighlightObject(true, HasMoreMeshes() ? combinedObject : this.gameObject);
     }
 
     public void OnTouchStop(Transform hand)
     {
-        HighlightObject(false);
+        HighlightObject(false, HasMoreMeshes() ? combinedObject : this.gameObject);
     }
 
-    private void HighlightObject(bool hightlight)
+    private void HighlightObject(bool hightlight, GameObject obj)
     {
-        foreach(MeshRenderer mr in GetComponentsInChildren<MeshRenderer>())
+        foreach (MeshRenderer mr in obj.GetComponentsInChildren<MeshRenderer>())
         {
             Material m = mr.material;
-            m.shader = hightlight ? Shader.Find("Outlined/Silhouetted Diffuse") : startShader;
+
+            if (!materialLibrary.ContainsKey(m))
+                materialLibrary.Add(m, m.shader);
+
+            m.shader = hightlight ? Shader.Find("Outlined/Silhouetted Diffuse") : materialLibrary[m];
             isHighlighted = hightlight;
         }
+    }
+
+    private Mesh GetCombinedMesh()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            i++;
+        }
+
+        Mesh combinedMesh = new Mesh();
+        combinedMesh.CombineMeshes(combine);
+
+        return combinedMesh;
+    }
+
+    private GameObject CreateCombinedObject()
+    {
+        GameObject g = new GameObject("Highlight object: " + this.gameObject.name);
+        g.AddComponent<MeshFilter>().mesh = GetCombinedMesh();
+        g.AddComponent<MeshRenderer>();
+        g.transform.localScale = transform.localScale;
+        g.transform.rotation = transform.rotation;
+        g.transform.position = transform.position;
+        g.transform.SetParent(this.transform);
+        return g;
+    }
+
+    private bool HasMoreMeshes()
+    {
+        Debug.Log("Mesh count: " + GetComponentsInChildren<MeshRenderer>().Length);
+        return GetComponentsInChildren<MeshRenderer>().Length > 1;
     }
 
     public void OnGrab(Transform hand)
